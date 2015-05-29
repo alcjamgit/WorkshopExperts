4cb9d1275da4cfaa73a852832863ebc5c7ca2054
alcjamgit <alcarazjames@gmail.com>
2015-05-29 22:27:23 +0800

    Init

---
+++ WorkshopExpert.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkshopExpert.Web.Startup))]
+namespace WorkshopExpert.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
