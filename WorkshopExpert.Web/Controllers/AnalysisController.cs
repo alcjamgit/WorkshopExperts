a089dec3c332a67f4107d35969f5f2604b65a700
alcjamgit <alcarazjames@gmail.com>
2015-06-02 05:30:04 +0800

    updating views

---
+++ WorkshopExpert.Web/Controllers/AnalysisController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using WorkshopExpert.Core.Entities;
+using WorkshopExpert.Infrastructure.DAL;
+using WorkshopExpert.Web.Models;
+using WorkshopExpert.Infrastructure.BLL.ApplicationServices;
+using System.Data.Entity;
+
+namespace WorkshopExpert.Web.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        // GET: Design
+        private readonly ApplicationDbContext _db;
+
+        public AnalysisController()
+        {
+            _db = new ApplicationDbContext(new CurrentUserService(System.Web.HttpContext.Current.User.Identity));
+        }
+
+        public ActionResult GetByAnalysisByWorkshopId(string id)
+        {
+            var modelId = new Guid(id);
+            var model = (from a in _db.Analyses
+                        where a.Workshop_Id == modelId
+                        select new AnalysisEditViewModel { 
+                            Id = a.Id,
+                            Workshop_Id = a.Workshop_Id,
+                            DurationHours = a.DurationHours,
+                            AttendeeProfile = a.AttendeeProfile,
+                            ProposedWorkshopTitle = a.ProposedWorkshopTitle,
+                            ProblemToOvercome = a.ProblemToOvercome,
+                            NeedToFulfill = a.NeedToFulfill
+                        }).FirstOrDefault();
+            return View(model);
+        }
+
+    }
+}
---
+++ WorkshopExpert.Web/Models/AnalysisViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopExpert.Web.Models
+{
+    public class AnalysisEditViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid Workshop_Id { get; set; }
+
+        public string ProposedWorkshopTitle { get; set; }
+        [UIHint("TimePicker")]
+        public decimal DurationHours { get; set; }
+        public string AttendeeProfile { get; set; }
+        public string ProblemToOvercome { get; set; }
+        public string NeedToFulfill { get; set; }
+
+    }
+
+}
